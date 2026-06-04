9aadde9a19cf29e89afa3801a2c457cd81d26bef
Joaquin-oc <joaquin.ocana@utec.edu.pe>
2026-06-03 23:22:04 -0500

    Refactor BuscarAlumno.py to handle JSON body

--- BuscarAlumno.py
+++ BuscarAlumno.py
@@ -1,8 +1,18 @@
 import boto3
+import json
+from decimal import Decimal
+
+class DecimalEncoder(json.JSONEncoder):
+    def default(self, obj):
+        if isinstance(obj, Decimal):
+            return str(obj)
+        return super().default(obj)
 
 def lambda_handler(event, context):
-    tenant_id = event['tenant_id']
-    alumno_id = event['alumno_id']
+    body = json.loads(event['body'])
+    
+    tenant_id = body['tenant_id']
+    alumno_id = body['alumno_id']
 
     dynamodb = boto3.resource('dynamodb')
     table = dynamodb.Table('t_alumnos')
@@ -12,7 +22,9 @@ def lambda_handler(event, context):
             'alumno_id': alumno_id
         }
     )
+    item = response.get('Item', {})
     return {
         'statusCode': 200,
-        'response': response
+        'headers': {'Access-Control-Allow-Origin': '*'},
+        'body': json.dumps({'statusCode': 200, 'item': item}, cls=DecimalEncoder)
     }
